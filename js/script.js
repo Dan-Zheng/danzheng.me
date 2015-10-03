e6cc677e401d009742a5ee1183e0ed74f12e4095
Dan Zheng <dan@Dan-Zhengs-MacBook-Pro.local>
2015-10-03 15:16:06 -0500

    Add html, css, js
    I'm demonstrating shit to jason

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 
-	var palette = ['#69D2E7','#A7DBD8','#F38630','#FA6900','#fe4365','#fc9d9a','#f9cdad','#556270','#4ecdc4','#c7f464','#ff6b6b','#c44d58','#d1e751','#ffffff','#4dbce9','#26ade4','#d95b43','#c02942','#542437','#53777a','#cff09e','#a8dba8','#79bd9a','#3b8686','#0b486b','#00a0b0','#6a4a3c','#cc333f','#eb6841','#edc951']
+	var palette = ['#69D2E7','#A7DBD8','#F38630','#FA6900','#fe4365','#fc9d9a','#f9cdad','#556270','#4ecdc4','#c7f464','#ff6b6b','#c44d58','#d1e751','#000000','#4dbce9','#26ade4','#d95b43','#c02942','#542437','#53777a','#cff09e','#a8dba8','#79bd9a','#3b8686','#0b486b','#00a0b0','#6a4a3c','#cc333f','#eb6841','#edc951']
 
 	// Will hold colors for gradient.
 	var color = [];
 	// Number of blocks
 	var numberOfBlocks = 5;
-	//randomNumber used to make random tiles. USED WITH UNSORT
-	var randomNumber = numberOfBlocks - 2;
+	//unsortNumber used to make random tiles. USED WITH UNSORT
+	var unsortNumber = numberOfBlocks - 2;
 	//Used to check players guess against actual
-	var playerGuess = ["#FF0088", "#0088ff"];
+	var playerGuess = [];
 	//Scores
 	var roundScore = 0;
 	var totalScore = 0;
@@ -18,7 +18,7 @@ $(document).ready(function(){
 
 	function makeBoard(colorStop){
 		numberOfBlocks = totalScore + 3;
-		randomNumber = numberOfBlocks - 2;
+		unsortNumber = numberOfBlocks - 2;
 		//Holds color-stops in a gradient
 		var colorStop = [palette[randomInt(palette.length - 1)], palette[randomInt(palette.length - 1)]];
 		color = [];
@@ -28,7 +28,7 @@ $(document).ready(function(){
 				$('#' + (100+i)).remove();
 			}
 		}
-		for (var i = 0; i < randomNumber; i++) {
+		for (var i = 0; i < unsortNumber; i++) {
 			$("#unsort").append($("<div class='unsort tile' id='" + i + "'></div>"));
 			$('#' + i).animate({'opacity': .8},1200);
 		}
@@ -38,11 +38,9 @@ $(document).ready(function(){
 			$('#' + (100 + i)).animate({'opacity': 1},500);
 		}
 
-		for(var i = 1; i <= (randomNumber); i++){
-			color.push($.xcolor.gradientlevel(colorStop[0], colorStop[1], i, numberOfBlocks).toHex());
+		for (var i = 1; i <= (unsortNumber); i++){
+			color.push($.xcolor.gradientlevel(colorStop[0], colorStop[1], i, numberOfBlocks).getHex());
 		}
-
-		console.log(color);
 
 		$('#100').css('background-color', colorStop[0]);
 		$('#100').removeClass("drop");
@@ -55,8 +53,8 @@ $(document).ready(function(){
 		var deck = shuffle(color);
 		color = tempColor;
 
-		for(var i = 0; i < (randomNumber); i++){
-			$("#"+i).css('background-color',deck[i]);
+		for (var i = 0; i < (unsortNumber); i++){
+			$("#"+i).css('background-color', deck[i]);
 		}
 
 		$(".unsort").draggable();
@@ -67,20 +65,26 @@ $(document).ready(function(){
 				console.log(color);
 				console.log(color[($(this).attr('id') - 101)]);
 				var rgb = $('#'+$(ui.draggable).attr('id')).css('background-color');
-				console.log( toHex(rgb) );
-				if(color[($(this).attr('id') - 101)] == toHex($('#'+$(ui.draggable).attr('id')).css('background-color')) ){
+				console.log(toHex(rgb));
+				if (color[($(this).attr('id') - 101)] == toHex($('#'+$(ui.draggable).attr('id')).css('background-color'))) {
 					$(this).css('background-color',color[($(this).attr('id') - 101)]);
 					$(ui.draggable).fadeOut(300);
 					roundScore++;
-					if(roundScore == (randomNumber)){
-						console.log(randomNumber);
+					if (roundScore == (unsortNumber)) {
+						console.log(unsortNumber);
 						$('#win').fadeIn(500);
 						totalScore++;
 						roundScore = 0;
-						$("#score").replaceWith("<div id='score'>Level: " + totalScore + "</div><div id='ng_con' class='game game-con'><div id='ng' class='game game-mess'>New Game</div>");
+						$("#score").replaceWith("<div id='score'>Level: " + totalScore + "</div><div id='ng_con' class='game game-con'><div id='ng' class='game game-button'>New Game</div>");
 					}
-				} else{
-					$(ui.draggable).animate({'top':'-=50'},50);
+				} else {
+
+					$(ui.draggable).animate({'opacity':'0.9'},0);
+					$(ui.draggable).animate({'top':'-=20'},40);
+					$(ui.draggable).animate({'top':'+=20'},40);
+					$(ui.draggable).animate({'top':'-=20'},40);
+					$(ui.draggable).animate({'top':'+=20'},40);
+					$(ui.draggable).animate({'opacity':'1'},0);
 				}
 			}
 		});
@@ -91,29 +95,37 @@ $(document).ready(function(){
 	 *
 	*/
 	function resize(numberOfBlocks, board){
-		var win_y = $(window).height();
-		var win_x = $(window).width();
+		var winY = $(window).height();
+		var winX = $(window).width();
 
-		var border = 10;
+		// var border = 5 - ((unsortNumber - 2) * 1.1);
+		var border = 5;
 
-		if (randomNumber > 6){
-			var board_width = win_x * .9;
-		} else {
-			var board_width = win_x * (randomNumber/7);
+		// var tileS = ((boardWidth - border) / numberOfBlocks - border);
+
+		if (board == 'sort') {
+			//console.log(unsortNumber);
+			var boardWidth = winX * .7;
+			var tileS = ((boardWidth - border) / numberOfBlocks - border);
+			//console.log('sort: ' + tileS);
+		} else if (board == 'unsort') {
+			var boardWidth = winX * .7 / (unsortNumber + 2) * unsortNumber;
+			var tileS = ((boardWidth - border) / numberOfBlocks - border); // FUCKING MATH WORK NOW
+			//console.log('unsort: ' + tileS);
 		}
-		var tile_s = (board_width - border) / numberOfBlocks - border;
-		var board_height = tile_s + 2 * border;
-		var margin_left = -board_width / 2;
+
+		var boardHeight = (tileS + 2 * border); // double border
+		var marginLeft = -boardWidth / 2;
 
 		$('#'+board).css({
-			"width": board_width + "px",
-			"height": board_height + "px",
-			"margin-left": margin_left + "px"
+			"width": boardWidth + "px",
+			"height": boardHeight + "px",
+			"margin-left": marginLeft + "px"
 		});
 
 		$('.tile.'+board).css({
-			"width": tile_s,
-			"height": tile_s,
+			"width": tileS,
+			"height": tileS,
 			"margin-left": border,
 			"margin-top": border,
 			"margin-bottom": border
@@ -121,7 +133,7 @@ $(document).ready(function(){
 	}
 
 	setInterval(function() {resize(numberOfBlocks, 'sort')}, 100);
-	setInterval(function() {resize(randomNumber, 'unsort')}, 100);
+	setInterval(function() {resize(unsortNumber, 'unsort')}, 100);
 
 	/*
 	 * Extension of styles
@@ -159,7 +171,10 @@ $(document).ready(function(){
 	  return array;
 	}
 
-	//Function to convert hex format to a rgb color
+	/* Converts color from RGB format to hex
+	 * @param rgb string storing color in RGB format
+	 * @return hex string storing color in hex format
+	 */
 	function toHex(rgb) {
 	 rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
 	 return (rgb && rgb.length === 4) ? "#" +
@@ -168,6 +183,9 @@ $(document).ready(function(){
 	  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
 	}
 
+	/* Generates random integer between 0 to n - 1
+	 * @return integer between 0 to n - 1, inclusive
+	 */
 	function randomInt(n) {
 		return Math.floor(Math.random() * n);
 	}
