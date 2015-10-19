75730bd2e05083e15b751bd8d32e2cb1bc7e4f47
Dan Zheng <zheng321@purdue.edu>
2015-10-19 17:00:36 -0400

    Modify style for gradients index.html
     * Add bootstrap css and js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+/* jshint -W004, -W033 */
+
 $(document).ready(function() {
 
-	var palette = ['#69d2e7','#a7dbd8','#f38630','#fa6900','#fe4365','#fc9d9a','#f9cdad','#556270','#4ecdc4','#c7f464','#ff6b6b','#c44d58','#d1e751','#000000','#4dbce9','#26ade4','#d95b43','#c02942','#542437','#53777a','#cff09e','#a8dba8','#79bd9a','#3b8686','#0b486b','#00a0b0','#6a4a3c','#cc333f','#eb6841','#edc951']
+	var palette = ['#69d2e7','#a7dbd8','#f38630','#fa6900','#fe4365','#fc9d9a','#f9cdad','#556270','#4ecdc4','#c7f464','#ff6b6b','#c44d58','#d1e751','#000000','#4dbce9','#26ade4','#d95b43','#c02942','#542437','#53777a','#cff09e','#a8dba8','#79bd9a','#3b8686','#0b486b','#00a0b0','#6a4a3c','#cc333f','#eb6841','#edc951'];
 
 	// Will hold colors for gradient.
 	var color = [];
@@ -23,8 +25,8 @@ $(document).ready(function() {
 		// Holds color-stops in a gradient
 		do {
 			var colorStop = [palette[randomInt(palette.length - 1)], palette[randomInt(palette.length - 1)]];
-			console.log(colorStop)
-		} while(colorStop[0] === colorStop[1])
+			console.log(colorStop);
+		} while(colorStop[0] === colorStop[1]);
 
 		color = [];
 
@@ -37,7 +39,7 @@ $(document).ready(function() {
 
 		for (var i = 0; i < unsortNumber; i++) {
 			$("#unsort").append($("<div class='unsort tile' id='" + i + "'></div>"));
-			$('#' + i).animate({'opacity': .8},1200);
+			$('#' + i).animate({'opacity': 0.8},1200);
 		}
 
 		for (var i = 0; i < (numberOfBlocks); i++) {
@@ -82,7 +84,7 @@ $(document).ready(function() {
 						$('#win').fadeIn(500);
 						totalScore++;
 						roundScore = 0;
-						$("#score").replaceWith("<div id='score'>Level: " + totalScore + "</div><div id='ng_con' class='game game-con'><div id='ng' class='game game-button'>New Game</div>");
+						$("#score").html("<div id='score'>Level: " + totalScore + "</div>");
 					}
 				} else {
 					$(ui.draggable).animate({'opacity':'0.9'},0);
@@ -105,17 +107,19 @@ $(document).ready(function() {
 
 		// var border = 5 - ((unsortNumber - 2) * 1.1);
 		var border = 5;
+		var boardWidth;
+		var tileS;
 
 		// var tileS = ((boardWidth - border) / numberOfBlocks - border);
 
 		if (board == 'sort') {
 			// console.log(unsortNumber);
-			var boardWidth = winX * .7;
-			var tileS = ((boardWidth - border) / numberOfBlocks - border);
+			boardWidth = winX * 0.7;
+			tileS = ((boardWidth - border) / numberOfBlocks - border);
 			// console.log('sort: ' + tileS);
 		} else if (board == 'unsort') {
-			var boardWidth = winX * .7 / (unsortNumber + 2) * unsortNumber;
-			var tileS = ((boardWidth - border) / numberOfBlocks - border);
+			boardWidth = winX * 0.7 / (unsortNumber + 2) * unsortNumber;
+			tileS = ((boardWidth - border) / numberOfBlocks - border);
 			// console.log('unsort: ' + tileS);
 		}
 
