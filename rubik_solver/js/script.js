ea3905cc15947dc7ca1caed0258ab8e5b018a6cd
Dan Zheng <zheng321@purdue.edu>
2015-10-19 03:33:05 -0400

    Modify index.html, script.js
     * Change css styling

--- rubik_solver/js/script.js
+++ rubik_solver/js/script.js
@@ -1,16 +1,21 @@
 /* Daniel Zheng
  * 16 Oct 2015
  * Rubik solver scramble demo
+ * TO DO: FIND A WAY TO DUMP PHP IMAGES
  */
 
 (function() {
     var start, progressHandle;
 
-    var cube = new Cube();
-
     var progress = function() {
         // add dot to represent progress
-        $('#status').text(function(index, text) { return text + '.'; });
+        $('#status').text(function(index, text) {
+            if(text.slice(-3) == '...') {
+                return text.slice(0, -3);
+            } else {
+                return text + '.';
+            }
+        });
     };
 
     var initialized = function() {
@@ -19,7 +24,7 @@
 
         var end = new Date(),
             duration = (end - start) / 1000;
-        $('#status').text('Initialization done in ' + duration + ' seconds.');
+        $('#status').text('Done! (' + duration + ' seconds)');
 
         // visualize scramble
         $('#randomState').css('visibility', 'visible');
@@ -41,20 +46,20 @@
             var urlOne = "http://cube.crider.co.uk/visualcube.png?size=300&alg=" + s;
             var urlTwo = "http://cube.crider.co.uk/visualcube.png?size=300&r=y225x34z180&alg=" + s;
             $('#randomState .resultText').html(alg);
-            $('#randomState .result').html("(Front view):<br><img src=\"" + urlOne + "\">" + "<br>(Back view):<br><img src=\"" + urlTwo + "\">");
+            $('#randomState .result').html("<br>(Front view):<br><img src=\"" + urlOne + "\">" + "<br><br>(Back view):<br><img src=\"" + urlTwo + "\">" + "<br><br>");
         });
-        };
+    };
 
-        var doAlg = function() {
-          // console.log($('#algInput')[0].value);
-          // perform moves in the move box
-          var algo = formatString($('#algInput')[0].value);
-          var algSpaceless = algo.replace(/\s+/g, ''); // remove spaces
-          var urlOne = "http://cube.crider.co.uk/visualcube.png?size=300&alg=" + algSpaceless;
-          var urlTwo = "http://cube.crider.co.uk/visualcube.png?size=300&r=y225x34z180&alg=" + algSpaceless;
-          $('#randomState .resultText').html(algo);
-          $('#randomState .result').html("(Front view):<br><img src=\"" + urlOne + "\">" + "<br>(Back view):<br><img src=\"" + urlTwo + "\">");
-        };
+    var doAlg = function() {
+        // console.log($('#algInput')[0].value);
+        // perform moves in the move box
+        var algo = formatString($('#algInput')[0].value);
+        var algSpaceless = algo.replace(/\s+/g, ''); // remove spaces
+        var urlOne = "http://cube.crider.co.uk/visualcube.png?size=300&alg=" + algSpaceless;
+        var urlTwo = "http://cube.crider.co.uk/visualcube.png?size=300&r=y225x34z180&alg=" + algSpaceless;
+        $('#randomState .resultText').html(algo);
+        $('#randomState .result').html("<br>(Front view):<br><img src=\"" + urlOne + "\">" + "<br><br>(Back view):<br><img src=\"" + urlTwo + "\">" + "<br><br>");
+    };
 
     var formatString = function(algString) {
         var result = '';
@@ -65,27 +70,27 @@
 
         var i = 0;
         while (i < algString.length) {
-          if (moveBasic.indexOf(algString[i]) != -1) {
-              if (i < algString.length - 1 && moveModify.indexOf(algString[i+1]) != -1) {
-                  result = result + ' ' + algString.slice(i,i+2) + ' ';
-                  i += 2;
-              } else {
-                  result = result + ' ' + algString[i];
-                  i += 1;
-              }
-          }
+            if (moveBasic.indexOf(algString[i]) != -1) {
+                if (i < algString.length - 1 && moveModify.indexOf(algString[i+1]) != -1) {
+                    result = result + ' ' + algString.slice(i,i+2) + ' ';
+                    i += 2;
+                } else {
+                    result = result + ' ' + algString[i];
+                    i += 1;
+                }
+            }
         }
         return result;
     };
 
     $(function() {
-        $('#status').text('Initializing');
+        $('#status').text('Loading');
 
         // begin measuring time
         start = new Date();
 
         // begin adding dots
-        progressHandle = setInterval(progress, 1000);
+        progressHandle = setInterval(progress, 500);
 
         // async precomputing
         Cube.asyncInit('cubejs/lib/worker.js', initialized);
