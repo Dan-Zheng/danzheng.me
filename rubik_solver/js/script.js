3af0a627a0370182a07ed21b696ac0db6febb1f1
Dan Zheng <zheng321@purdue.edu>
2015-10-19 04:53:35 -0400

    Make input box randomly generate pattern algorithm
     * Modify index.html and script.js

--- rubik_solver/js/script.js
+++ rubik_solver/js/script.js
@@ -30,6 +30,20 @@
         $('#randomState').css('visibility', 'visible');
         $('#scramble').on('click', generateScramble);
         $('#doAlg').on('click', doAlg);
+
+        // make random pattern algorithm in input
+        var algList = ["F B2 R’ D2 B R U D’ R L’ D’ F’ R2 D F2 B’",
+                       "U2 D2 F2 B2 L2 R2",
+                       "U F B’ L2 U2 L2 F’ B U2 L2 U",
+                       "R2 L’ D F2 R’ D’ R’ L U’ D R D B2 R’ U D2",
+                       "F L F U’ R U F2 L2 U’ L’ B D’ B’ L2 U",
+                       "U’ L’ U’ F’ R2 B’ R F U B2 U B’ L U’ F U R F’",
+                       "L U B’ U’ R L’ B R’ F B’ D R D’ F’",
+                       "U D’ R L’ F B’ U D’",
+                       "F R’ U L F’ L’ F U’ R U L’ U’ L F’",
+                       "F B2 R’ D2 B R U D’ R L’ D’ F’ R2 D F2 B’ U D’ R L’ F B’ U D’"
+                    ];
+        $('input[id=algInput]').val(algList.randomElement());
     };
 
     var generateScramble = function() {
@@ -68,7 +82,11 @@
         var moveBasic = ['U', 'D', 'F', 'B', 'L', 'R', 'u', 'd', 'f', 'b', 'l', 'r'];
         var moveModify = ['2', '\''];
 
+        console.log(algString);
         algString = algString.replace(/\s+/g, ''); // remove spaces
+        console.log(algString);
+        algString = algString.replace(/\u2019+/g, '\''); // replace utf8 right quotation with ASCII quotation
+        console.log(algString);
 
         var i = 0;
         while (i < algString.length) {
@@ -83,6 +101,10 @@
             }
         }
         return result;
+    };
+
+    Array.prototype.randomElement = function () {
+        return this[Math.floor(Math.random() * this.length)];
     };
 
     $(function() {
