020b63ee08f69d9658c14d8d41c6f64f8ba91661
Dan Zheng <zheng321@purdue.edu>
2015-10-25 06:42:04 -0400

    Change button functionality, update css

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 	<script src="bower_components/jquery.pep/src/jquery.pep.js"></script>
 	<script src="bower_components/xcolor/jquery.xcolor.min.js"></script>
 	<script src='bower_components/bootstrap/dist/js/bootstrap.min.js'></script>
-	<script src="home/js/script.js"></script>
+	<script src="js/index.js"></script>
 </head>
 
 <?php include("header.php");?>
@@ -26,10 +26,9 @@
     <div class="jumbotron text-center">
         <div class="container" id='main'>
             <h1>Hello, world!</h1>
-            <p id='subtitle'>This website is a repository for projects by <a href="http://github.com/Dan-Zheng">Dan Zheng</a>.</p>
-            <p>Try using the navbar!</p>
+            <p class='section-module' id='subtitle'>This website is a repository for projects by <a href="http://github.com/Dan-Zheng">Dan Zheng</a>.</p>
             <!--<p class='text-center' id='liga'>->> >> -> <=> ==> >>= -- := =:= == != <= >= // /** /* */ && .& || !! :: >> __ ___ .. ...</p>-->
-            <p><a class="btn btn-primary btn-lg" href="#" role="button">Useless button!</a></p>
+            <p class='section-module'><a class="btn btn-primary btn-lg" href="#" role="button" id='randomProject'>Random Project!</a></p>
             <ul class="list-inline social-buttons">
                 <li><a href="https://github.com/Dan-Zheng" target="_blank"><i class="fa fa-github"></i></a>
                 </li>
--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
     console.log("windowHeight: " + $( window ).height() + "px");
     console.log("windowWidth: " + $( window ).width() + "px");
+
+    $('#randomProject').on('click', function() {
+        var projects = ["gradients",
+                    "rubik-solver",
+                    "floats-your-boat",
+                    "happy-libs",
+    ];
+        var project = projects[Math.floor(Math.random() * projects.length)];
+        console.log(project);
+        window.location = "http://danzheng.me/" + project + ".php";
+    });
 });
