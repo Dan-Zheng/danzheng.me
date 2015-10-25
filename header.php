6ba7368d3cc83b98567b222baf06e5860a3bab98
Dan Zheng <zheng321@purdue.edu>
2015-10-25 05:20:32 -0400

    Edit header.php
     * Simple format change

--- header.php
+++ header.php
@@ -14,9 +14,9 @@
             </div>
             <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
                 <ul class='nav navbar-nav navbar-right'>
-                    <li><a href='./index.html'>Home</a></li><!--
-                    <li><a id='nav-about' href='#about-section'>About</a></li>
-                    <li><a id='nav-contact' href='#contact-section'>Contact</a></li>-->
+                    <li><a href='./index.html'>Home</a></li>
+                    <li><a id='nav-about' href='#about-section'>About</a>
+                    <li><a id='nav-contact' href='#contact-section'>Contact</a></li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Projects<span class="caret"></span></a>
                         <ul class="dropdown-menu">
@@ -25,11 +25,8 @@
                             <li><a id='nav-floats' href='floats-your-boat/index.html'>Floats Your Boat</a></li>
                             <li role="separator" class="divider"></li>
                             <li><a id='nav-libs' href='happy-libs/index.html'>Happy Libs</a></li>
-                            <!--<li role="separator" class="divider"></li>-->
                         </ul>
                     </li>
-                    <li><a id='nav-about' href='#about-section'>About</a>
-                    <li><a id='nav-contact' href='#contact-section'>Contact</a></li>
                 </ul>
             </div>
         </div>
