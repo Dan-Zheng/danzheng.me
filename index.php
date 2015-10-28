d6d7cd065bafccc1d77b22763aab2335efe98d22
Dan Zheng <zheng321@purdue.edu>
2015-10-28 15:14:24 -0400

    Fix font formatting issue

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0">
     <link rel='stylesheet' href='bower_components/bootstrap/dist/css/bootstrap.min.css'>
     <link rel='stylesheet' href='bower_components/font-awesome/css/font-awesome.min.css'>
-    <link href='https://fonts.googleapis.com/css?family=Roboto:100.300,400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/header.css" type="text/stylesheet">
     <link rel="stylesheet" href="css/fonts.css" type="text/stylesheet">
     <link rel="stylesheet" href="css/index.css" type="text/stylesheet">
@@ -22,16 +22,18 @@
 <body>
     <div class="jumbotron text-center">
         <div class="container" id='main'>
-            <h1>Hello, world!</h1>
-            <p class='section-module' id='subtitle'>This website is a repository for projects by <a href="http://github.com/Dan-Zheng">Dan Zheng</a>.</p>
-            <!--<p class='text-center' id='liga'>->> >> -> <=> ==> >>= -- := =:= == != <= >= // /** /* */ && .& || !! :: >> __ ___ .. ...</p>-->
-            <p class='section-module'><a class="btn btn-primary btn-lg" href="#" role="button" id='randomProject'>Random Project!</a></p>
-            <ul class="list-inline social-buttons">
-                <li><a href="https://github.com/Dan-Zheng" target="_blank"><i class="fa fa-github"></i></a>
-                </li>
-                <li><a href="https://www.facebook.com/VoidQuery" target="_blank"><i class="fa fa-facebook"></i></a>
-                </li>
-            </ul>
+            <div class='row'>
+                <h1>Hello, world!</h1>
+                <p class='section-module' id='subtitle'>This website is a repository for projects by <a href="http://github.com/Dan-Zheng">Dan Zheng</a>.</p>
+                <!--<p class='text-center' id='liga'>->> >> -> <=> ==> >>= -- := =:= == != <= >= // /** /* */ && .& || !! :: >> __ ___ .. ...</p>-->
+                <p class='section-module'><a class="btn btn-primary btn-lg" href="#" role="button" id='randomProject'>Random Project!</a></p>
+                <ul class="list-inline social-buttons">
+                    <li><a href="https://github.com/Dan-Zheng" target="_blank"><i class="fa fa-github"></i></a>
+                    </li>
+                    <li><a href="https://www.facebook.com/VoidQuery" target="_blank"><i class="fa fa-facebook"></i></a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
     <div class='container' id='projects'>
