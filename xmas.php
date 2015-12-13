6c2c6e1c2c29f1cc570b25bf5ae0acd78b2d0dfe
Dan Zheng <zheng321@purdue.edu>
2015-12-13 02:05:59 -0500

    Add xmas

---
+++ xmas.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Dan Zheng</title>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href='terminal.ico'/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0">
+    <link rel='stylesheet' href='bower_components/bootstrap/dist/css/bootstrap.min.css'>
+    <link rel='stylesheet' href='bower_components/font-awesome/css/font-awesome.min.css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/header.css" type="text/stylesheet">
+    <link rel="stylesheet" href="css/fonts.css" type="text/stylesheet">
+    <link rel="stylesheet" href="css/index.css" type="text/stylesheet">
+    <link rel="stylesheet" href="css/xmas.css" type="text/stylesheet">
+
+	<script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
+	<script type="text/javascript" src='bower_components/bootstrap/dist/js/bootstrap.min.js'></script>
+	<script type="text/javascript" src="js/index.js"></script>
+</head>
+
+<?php include("header.php");?>
+
+<body>
+    <div class="xmasTree"></div>
+</body>
